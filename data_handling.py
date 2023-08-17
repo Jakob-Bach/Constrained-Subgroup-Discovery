b4d3eb830100b4c131324499002fc5dc12558809
Jakob Bach <jakob.bach@kit.edu>
2023-08-17 16:39:28 +0200

    Add demo dataset preparation
    No interesting benchmark datasets yet, but some small datasets integrated
    into scikit-learn. Besides a script that saves the datasets in an
    appropriate format for the experimental pipeline, we also add general
    data-handling functionality as a separate module (not all functionality
    used yet; copied with minor adaptations from AFS project).

---
+++ data_handling.py
@@ -0,0 +1,63 @@
+"""Data handling
+
+Functions for data I/O in the experimental pipeline (prediction datasets and experimental data).
+"""
+
+
+import pathlib
+from typing import Optional, Sequence, Tuple
+
+import pandas as pd
+
+
+# Feature-part and target-part of a dataset are saved separately.
+def load_dataset(dataset_name: str, directory: pathlib.Path) -> Tuple[pd.DataFrame, pd.Series]:
+    X = pd.read_csv(directory / (dataset_name + '_X.csv'))
+    y = pd.read_csv(directory / (dataset_name + '_y.csv')).squeeze(axis='columns')
+    assert isinstance(y, pd.Series)  # a DataFrame might cause errors somewhere in the pipeline
+    return X, y
+
+
+def save_dataset(X: pd.DataFrame, y: pd.Series, dataset_name: str, directory: pathlib.Path) -> None:
+    X.to_csv(directory / (dataset_name + '_X.csv'), index=False)
+    y.to_csv(directory / (dataset_name + '_y.csv'), index=False)
+
+
+# List dataset names based on target-values_files.
+def list_datasets(directory: pathlib.Path) -> Sequence[str]:
+    return [file.name.split('_y.')[0] for file in list(directory.glob('*_y.*'))]
+
+
+def load_dataset_overview(directory: pathlib.Path) -> pd.DataFrame:
+    return pd.read_csv(directory / '_dataset_overview.csv')
+
+
+def save_dataset_overview(dataset_overview: pd.DataFrame, directory: pathlib.Path) -> None:
+    dataset_overview.to_csv(directory / '_dataset_overview.csv', index=False)
+
+
+# Return the path of the file containing either complete experimental results or only a particular
+# combination of dataset, fold, and subgroup-discovery method.
+def get_results_file_path(directory: pathlib.Path, dataset_name: Optional[str] = None,
+                          split_idx: Optional[int] = None, sd_name: Optional[str] = None) -> pathlib.Path:
+    if (dataset_name is not None) and (split_idx is not None) and (sd_name is not None):
+        return directory / (f'{dataset_name}_{split_idx}_{sd_name}_results.csv')
+    return directory / '_results.csv'
+
+
+# Load either complete results or only a particular combi of dataset, fold, and subgroup discovery.
+def load_results(directory: pathlib.Path, dataset_name: Optional[str] = None,
+                 split_idx: Optional[int] = None, sd_name: Optional[str] = None) -> pd.DataFrame:
+    results_file = get_results_file_path(directory=directory, dataset_name=dataset_name,
+                                         split_idx=split_idx, sd_name=sd_name)
+    if results_file.exists():
+        return pd.read_csv(results_file)
+    # If particular results file does not exist, just grab and merge all results in the directory:
+    return pd.concat([pd.read_csv(x) for x in directory.glob('*_results.*')], ignore_index=True)
+
+
+def save_results(results: pd.DataFrame, directory: pathlib.Path, dataset_name: Optional[str] = None,
+                 split_idx: Optional[int] = None,  sd_name: Optional[str] = None) -> None:
+    results_file = get_results_file_path(directory=directory, dataset_name=dataset_name,
+                                         split_idx=split_idx, sd_name=sd_name)
+    results.to_csv(results_file, index=False)
---
+++ prepare_demo_datasets.py
@@ -0,0 +1,68 @@
+"""Prepare demo datasets
+
+Script to minimally preprocess and save a few datasets from scikit-learn (toy and synthetically
+generated), so they can be used in the experimental pipeline.
+The actual experiments for the paper use different datasets.
+
+Usage: python -m prepare_demo_datasets --help
+"""
+
+
+import argparse
+import itertools
+import pathlib
+
+import pandas as pd
+import sklearn.datasets
+import tqdm
+
+import data_handling
+
+
+SYN_DATASETS_N_INSTANCES = [100, 200, 400]
+SYN_DATASETS_N_FEATURES = [10, 20, 40]
+TOY_DATASETS_NAMES = ['breast_cancer', 'digits', 'iris', 'wine']
+
+
+# Main-routine: download, pre-process, and save (to "data_dir") datasets from PMLB.
+def prepare_datasets(data_dir: pathlib.Path) -> None:
+    if not data_dir.is_dir():
+        print('Dataset directory does not exist. We create it.')
+        data_dir.mkdir(parents=True)
+    if any(data_dir.iterdir()):
+        print('Dataset directory is not empty. Files might be overwritten, but not deleted.')
+    dataset_overview = []
+
+    print('Saving toy datasets ...')
+    for dataset_name in tqdm.tqdm(TOY_DATASETS_NAMES):
+        X, y = getattr(sklearn.datasets, 'load_' + dataset_name)(as_frame=True, return_X_y=True)
+        data_handling.save_dataset(X=X, y=y, dataset_name=dataset_name, directory=data_dir)
+        dataset_overview.append({'dataset': dataset_name, 'n_instances': X.shape[0],
+                                 'n_features': X.shape[1]})
+
+    print('Saving synthetically generated datasets ...')
+    for n_instances, n_features in tqdm.tqdm(list(itertools.product(SYN_DATASETS_N_INSTANCES,
+                                                                    SYN_DATASETS_N_FEATURES))):
+        X, y = sklearn.datasets.make_classification(n_samples=n_instances, n_features=n_features,
+                                                    n_classes=2, random_state=25)
+        X = pd.DataFrame(X, columns=[f'Feature_{i + 1}' for i in range(n_features)])
+        y = pd.Series(y, name='target')
+        dataset_name = f'syn_{n_instances}_{n_features}'
+        data_handling.save_dataset(X=X, y=y, dataset_name=dataset_name, directory=data_dir)
+        dataset_overview.append({'dataset': dataset_name, 'n_instances': X.shape[0],
+                                 'n_features': X.shape[1]})
+
+    data_handling.save_dataset_overview(pd.DataFrame(dataset_overview), directory=data_dir)
+
+
+# Parse some command-line arguments and run the main routine.
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Retrieves small demo datasets from scikit-learn, prepares them for the ' +
+        'experimental pipeline, and stores them in the specified directory.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-d', '--directory', type=pathlib.Path, default='data/datasets/',
+                        dest='data_dir', help='Directory to store prediction datasets.')
+    print('Dataset preparation started.')
+    prepare_datasets(**vars(parser.parse_args()))
+    print('Datasets prepared and saved.')
