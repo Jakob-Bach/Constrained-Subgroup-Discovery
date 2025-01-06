5c072089df922556bd264f6fe548ecf3a1265c6b
Jakob Bach <jakob.bach@kit.edu>
2025-01-06 14:06:16 +0100

    Add pysubgroup runtime competitors
    One heuristic search method, three exhaustive ones.

--- runtime_competitors.py
+++ runtime_competitors.py
@@ -7,15 +7,18 @@ Wrappers for subgroup-discovery methods in the runtime-competitor experiments.
 from abc import abstractmethod, ABCMeta
 import time
 from typing import Any, Optional, Type
+import warnings
 
 import pandas as pd
 import pysubdisc
+import pysubgroup
 import sd4py
 import subgroups
 
 import csd
 
 
+BEAM_WIDTH = 10  # for beam search; consistent with our own default beam width
 NUM_BINS = 50  # for methods that require discretization of features
 
 
@@ -67,20 +70,22 @@ class PysubdiscMethod(SDMethod):
         self._search_strategy = search_strategy
 
     def discover_subgroup(self, X: pd.DataFrame, y: pd.Series, k: Optional[int]) -> Any:
+        if k is None:
+            k = X.shape[1]
         data = pd.concat((X, y.astype(str)), axis='columns')
         model = pysubdisc.singleNominalTarget(data, targetColumn='target', targetValue='1')
         # Model parameters explained in https://github.com/SubDisc/SubDisc/wiki and
         # https://github.com/SubDisc/SubDisc/blob/main/manual/manual.pdf
         model.minimumCoverage = 1  # lower bound for subgroup size (absolute)
         model.maximumCoverageFraction = 1.0  # upper bound for subgroup size (relative)
-        model.maximumSubgroups = 10  # according to manual, not only final but also intermediate
+        model.maximumSubgroups = BEAM_WIDTH  # according to manual, also for intermediate solutions
         model.nrThreads = 1
         model.numericStrategy = 'NUMERIC_BEST'  # enumerate all possible split points
         model.qualityMeasure = 'WRACC'
         model.qualityMeasureMinimum = -float('inf')
-        model.searchDepth = k if k is not None else X.shape[1]  # selected features <= search depth
+        model.searchDepth = k  # selected features <= search depth
         model.searchStrategy = self._search_strategy
-        model.searchStrategyWidth = 10  # as in our beam search; diff to "maximumSubgroups" unclear
+        model.searchStrategyWidth = BEAM_WIDTH  # difference to "maximumSubgroups" unclear
         model.run(verbose=False)
 
 
@@ -108,6 +113,55 @@ class Pysubdisc_DepthFirst(PysubdiscMethod):
         super().__init__(search_strategy='DEPTH_FIRST')
 
 
+class PysubgroupMethod(SDMethod):
+
+    def __init__(self, model: Any):
+        super().__init__()
+        self._model = model  # don't have a common superclass, but all have execute() method
+
+    def discover_subgroup(self, X: pd.DataFrame, y: pd.Series, k: Optional[int]) -> Any:
+        if k is None:
+            k = X.shape[1]
+        data = pd.concat((X, y), axis='columns')
+        target = pysubgroup.BinaryTarget(target_attribute='target', target_value=1)
+        search_space = []
+        for feature in X.columns:
+            for value in X[feature].unique():
+                search_space.append(pysubgroup.IntervalSelector(feature, value, float('inf')))
+                search_space.append(pysubgroup.IntervalSelector(feature, float('-inf'), value))
+        task = pysubgroup.SubgroupDiscoveryTask(
+            data=data, target=target, search_space=search_space, result_set_size=1,
+            qf=pysubgroup.WRAccQF(), depth=k  # selected features <= search depth
+        )
+        with warnings.catch_warnings():
+            warnings.filterwarnings(action="ignore", message="invalid value encountered in divide")
+            self._model.execute(task=task)
+
+
+class Pysubgroup_Apriori(PysubgroupMethod):
+
+    def __init__(self):
+        super().__init__(model=pysubgroup.Apriori())
+
+
+class Pysubgroup_BeamSearch(PysubgroupMethod):
+
+    def __init__(self):
+        super().__init__(model=pysubgroup.BeamSearch(beam_width=BEAM_WIDTH))
+
+
+class Pysubgroup_GpGrowth(PysubgroupMethod):
+
+    def __init__(self):
+        super().__init__(model=pysubgroup.GpGrowth())
+
+
+class Pysubgroup_SimpleDFS(PysubgroupMethod):
+
+    def __init__(self):
+        super().__init__(model=pysubgroup.SimpleDFS())
+
+
 class SD4PyMethod(SDMethod):
 
     def __init__(self, method: str):
@@ -122,7 +176,7 @@ class SD4PyMethod(SDMethod):
         # mandatory; "k=10" is number of solutions; "max_selectors" counts number of features
         sd4py.discover_subgroups(
             ontology=data, target='target', target_value=1, qf='ps', minqual=-float('inf'),
-            method=self._method, nbins=NUM_BINS, k=10, max_selectors=k
+            method=self._method, nbins=NUM_BINS, k=BEAM_WIDTH, max_selectors=k
         )
 
 
@@ -153,7 +207,7 @@ class Subgroups_BSD(SDMethod):
         model = subgroups.algorithms.BSD(
             min_support=0, quality_measure=subgroups.quality_measures.WRAcc(),
             optimistic_estimate=subgroups.quality_measures.WRAccOptimisticEstimate1(),
-            num_subgroups=10, max_depth=k
+            num_subgroups=BEAM_WIDTH, max_depth=k
         )
         model.fit(pandas_dataframe=data, tuple_target_attribute_value=('target', '1'))
 
